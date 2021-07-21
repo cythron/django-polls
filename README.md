55ee9cb3733047d4d1d191dc7cac1f272c4fa708
cythron <comtruise10@gmail.com>
2021-07-21 21:54:49 +0545

    Provide skel for environment vars

---
+++ README.md
@@ -0,0 +1,4 @@
+# Running the Web Application
+
+- Edit env_skel.sh
+- Source env_skel.sh
---
+++ env_skel.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+export POSTGRES_USERNAME=
+export POSTGRES_PASSWORD=
+export POSTGRES_DB=
+export POSTGERES_PORT=
+export POSTGRES_HOST=
